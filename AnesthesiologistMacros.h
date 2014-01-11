f298ede1ce80348a5448f205b581a616e43e8688
partounian <partounian@gmail.com>
2014-01-11 13:44:34 -0800

    Updated macros with macros needed for launcher.

--- AnesthesiologistMacros.h
+++ AnesthesiologistMacros.h
@@ -9,10 +9,12 @@
 #define DRIVE_REAR_LEFT_MOTOR_CHANNEL           (2)//TODO: dummy number
 #define DRIVE_FRONT_RIGHT_MOTOR_CHANNEL         (3)//TODO: dummy number
 #define DRIVE_REAR_RIGHT_MOTOR_CHANNEL          (4)//TODO: dummy number
+#define LAUNCHER_MOTOR_VICTOR_CHANNEL           (5)//TODO: dummy number
 
 	//Pneumatics
 #define SHIFTER_SOLENOID_CHANNEL_A				(1)//TODO: dummy number
 #define SHIFTER_SOLENOID_CHANNEL_B				(2)//TODO: dummy number
+#define LAUNCHER_SOLENOID_CHANNEL				(3)//TODO: dummy number
 
 	//Manipulators
 #define INTAKE_ROLLER_VICTOR_CHANNEL			(1)//TODO: dummy number
