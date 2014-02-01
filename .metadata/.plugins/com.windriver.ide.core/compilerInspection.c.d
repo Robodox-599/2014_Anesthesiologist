fa05287df45e40d191538a055a3b0b1f54608808
partounian <partounian@gmail.com>
2014-02-01 11:27:15 -0800

    Restructuring project
    .workspace is windriver's workspace directory, we shouldn't be committing that because it'll vary from machine to machine.
    
    Instead:
      Create a workspace (Ex: My Documents\WindriverWorkspace)
      Use File > Import, Select General > "Existing Projects into Workspace"
      Select this repository

--- .metadata/.plugins/com.windriver.ide.core/compilerInspection.c.d
+++
@@ -1,2 +0,0 @@
-compilerInspection.o:  \
- C:/2014_Alchemist/.metadata/.plugins/com.windriver.ide.core/compilerInspection.c
--- .metadata/.plugins/com.windriver.ide.core/compilerInspection.cpp.d
+++
@@ -1,2 +0,0 @@
-compilerInspection.o:  \
- C:/Users/Teacher/Documents/GitHub/2014_Anesthesiologist/.metadata/.plugins/com.windriver.ide.core/compilerInspection.cpp
